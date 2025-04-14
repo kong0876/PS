20cdf7acc84e17fcef0da5468457035c8a81d229
Joonyoung_ <72405637+kong0876@users.noreply.github.com>
2025-04-14 19:58:47 +0900

    [Silver II] Title: 점수 계산, Time: 528 ms, Memory: 134432 KB -BaekjoonHub

---
+++ 백준/Silver/25710. 점수 계산/점수 계산.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+// [25710 점수 계산] 
+
+public class Main {
+
+	static int n;
+	static HashSet<Integer> set1 = new HashSet<>();
+	static HashSet<Integer> set2 = new HashSet<>();
+	static ArrayList<Integer> arr = new ArrayList<>();
+	static int res;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		n = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int input = Integer.parseInt(st.nextToken());
+			if (!set1.contains(input)) {
+				set1.add(input);
+			} else {
+				set2.add(input);
+			}
+		}
+
+		for (int n : set1) {
+			arr.add(n);
+		}
+
+		for (int n : set2) {
+			arr.add(n);
+		}
+
+//		System.out.println(arr);
+		
+		int res = 0;
+		
+		for (int i = 0; i < arr.size() - 1; i++) {
+			for (int j = i + 1; j < arr.size(); j++) {
+				String str = Integer.toString(arr.get(i)*arr.get(j));
+				int tempRes = 0;
+				for(int k = 0; k < str.length(); k++) {
+					tempRes+= str.charAt(k) - '0';
+				}
+				
+				res = Math.max(res, tempRes);
+			}
+		}
+
+		System.out.println(res);
+
+	}
+}
