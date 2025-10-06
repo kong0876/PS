d476e0a81fe3b05ed4e5d64328ee77f75cb701fc
Joonyoung_ <72405637+kong0876@users.noreply.github.com>
2025-10-06 11:28:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,32 +1,32 @@
 import java.util.*;
 
-// System.out.println();
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[]{0, 0}; // 번호, 차례
+        int[] answer = new int[] {0,0} ; // [탈락한 사람 번호, 탈락 차례]
 
-        HashSet<String> set = new HashSet<>();
-        
         int cnt = 0;
-        char last = words[0].charAt(words[0].length() - 1);
-        set.add(words[0]);
+        char last = words[0].charAt(0);
+        HashSet<String> used = new HashSet<>();
         
-        for(int i = 1; i < words.length; i++){
-            String word = words[i];
-            
-            if(set.contains(word) || word.charAt(0)!= last){
-                cnt = i;
-                break;   
+        for(String word : words){
+            if(used.contains(word) || word.charAt(0) != last) {
+                System.out.println("break");
+                break;
             }
-
-            set.add(word);
+            
+            used.add(word);
             last = word.charAt(word.length() - 1);
-               
+            cnt ++;
+            
         }
-        if(cnt != 0){
-            answer[0] = (cnt % n) + 1;
-            answer[1] = (cnt / n) + 1;
+        
+        if(cnt == words.length){
+            answer[0] = 0;
+            answer[1] = 0;
+        } else{
+            answer[0] = cnt % n + 1;
+            answer[1] = cnt / n + 1;
         }
 
         return answer;
